76b818fa808a3d51d086b1b3d8035d7261c91627
Kevin Kelley <kkelley622@gmail.com>
2021-12-21 17:54:40 -0600

    commit tree fe766faeec938ee5169b95241b3a2d1d65fb4edc

---
+++ index.js
@@ -0,0 +1,21 @@
+// has a function expression called divide
+
+const divide = function () {
+    return 2000/100;
+}
+
+const square = x => x **2;
+
+const add = (y, z) => y + z;
+
+
+
+
+
+
+
+
+
+
+
+
